6aa2d34ed788823eca49bdf12a79ce0d0a5a9197
Rajapandian Chellimuthu <rajapandianc@outlook.in>
2022-08-18 07:10:30 +0530

    Chain validation, replacement and log stub in tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,13 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
+  let blockchain, newChain, originalChain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+    newChain = new Blockchain();
+    originalChain = blockchain.chain;
+  });
 
   it("is an instance of an Array", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -13,9 +19,110 @@ describe("Blockchain", () => {
   });
 
   it("adds new block to the blockchain", () => {
-    const lastBlock = blockchain.chain[blockchain.chain.length - 1];
     const data = "new-data";
-    blockchain.addBlock({ lastBlock, data });
+    blockchain.addBlock({ data });
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+  });
+
+  describe("isValidChain()", () => {
+    describe("when the chain does not start with a genesis block", () => {
+      it("returns false", () => {
+        blockchain.chain[0] = { data: "fake-genesis" };
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("when the chain starts with a genesis block and has multiple blocks", () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: "Block 1 data" });
+        blockchain.addBlock({ data: "Block 2 data" });
+        blockchain.addBlock({ data: "Block 3 data" });
+      });
+      describe("and a lastHash reference has changed", () => {
+        it("returns false", () => {
+          blockchain.chain[2].lastHash = "broken-lastHash";
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain contains a block with an invalid field", () => {
+        it("returns false", () => {
+          blockchain.chain[2].data = "bad-data";
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain does not contain invalid blocks", () => {
+        it("returns true", () => {
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
+  });
+
+  describe("replaceChain()", () => {
+    let errorMock, logMock;
+
+    beforeEach(() => {
+      errorMock = jest.fn();
+      logMock = jest.fn();
+
+      global.console.error = errorMock;
+      global.console.log = logMock;
+    });
+
+    describe("when the new chain is not longer", () => {
+      beforeEach(() => {
+        newChain.chain[0] = { new: "chain" };
+        blockchain.replaceChain(newChain.chain);
+      });
+
+      it("does not replace the chain", () => {
+        expect(blockchain.chain).toEqual(originalChain);
+      });
+
+      it("logs an error", () => {
+        expect(errorMock).toHaveBeenCalled();
+      });
+    });
+
+    describe("when the new chain is longer", () => {
+      beforeEach(() => {
+        newChain.addBlock({ data: "Block 1 data" });
+        newChain.addBlock({ data: "Block 2 data" });
+        newChain.addBlock({ data: "Block 3 data" });
+      });
+
+      describe("and the chain is invalid", () => {
+        beforeEach(() => {
+          newChain.chain[2].hash = "bad-hash";
+          blockchain.replaceChain(newChain.chain);
+        });
+
+        it("does not replace the chain", () => {
+          expect(blockchain.chain).toEqual(originalChain);
+        });
+
+        it("logs an error", () => {
+          expect(errorMock).toHaveBeenCalled();
+        });
+      });
+
+      describe("and the chain is valid", () => {
+        beforeEach(() => {
+          blockchain.replaceChain(newChain.chain);
+        });
+
+        it("replaces the chain", () => {
+          expect(blockchain.chain).toEqual(newChain.chain);
+        });
+
+        it("logs about the chain replacement", () => {
+          expect(logMock).toHaveBeenCalled();
+        });
+      });
+    });
   });
 });
